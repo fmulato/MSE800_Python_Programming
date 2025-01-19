b468cad2da7c693c6d40ec684011ddc6cb937014
FABRICIO MELLO MULATO <95538735+fmulato@users.noreply.github.com>
2025-01-20 12:13:32 +1300

    Update main.py

--- main.py
+++ main.py
@@ -1,28 +1,27 @@
 class Customer:
-    def __init__(self, customer_id, name="", email="", phone=""): #valores default para nao dar erro caso seja chamado sem parametros
-        self._customer_id = customer_id  # Encapsulation using _
+    def __init__(self, customer_id, name="", email="", phone=""):
+        self._customer_id = customer_id
         self._name = name
         self._email = email
         self._phone = phone
 
-    def create_user(self): #nome corrigido
+    def create_user(self):
         self._name = input("Input your full name: ")
         self._email = input("Input your email: ")
         self._phone = input("Input your phone number: ")
 
-    def display_info(self): #metodo para exibir as informações
+    def display_info(self):
         print(f"ID: {self._customer_id}")
         print(f"Name: {self._name}")
         print(f"Email: {self._email}")
         print(f"Phone: {self._phone}")
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     try:
-        customer_id = int(input("Input your customer ID: ")) #recebe o id do usuário
-        cst = Customer(customer_id) #instancia o objeto com o id
-        cst.create_user() #chama a função para receber os outros dados
-        cst.display_info() #chama a função para exibir os dados
+        customer_id = int(input("Input your customer ID: "))
+        cst = Customer(customer_id)
+        cst.create_user()
+        cst.display_info()
     except ValueError:
         print("Invalid customer ID. Please enter a number.")
