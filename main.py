383cfd9c0972346852e908f81b6ce246588a5803
fmulato <fmulato@yahoo.com>
2025-01-20 12:20:10 +1300

    first version of the code

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import json
+
 class Customer:
     def __init__(self, customer_id, name="", email="", phone=""):
         self._customer_id = customer_id
@@ -16,12 +18,24 @@ class Customer:
         print(f"Email: {self._email}")
         print(f"Phone: {self._phone}")
 
+    def save_json_file(self):
+        data = {
+            "customer_id": self._customer_id,
+            "name": self._name,
+            "email": self._email,
+            "phone": self._phone
+        }
+        print(f"Data saved successfully")
+        with open("customer.json", "w") as file:
+            json.dump(data, file)
+
 
 if __name__ == '__main__':
     try:
         customer_id = int(input("Input your customer ID: "))
         cst = Customer(customer_id)
         cst.create_user()
+        cst.save_json_file()
         cst.display_info()
     except ValueError:
         print("Invalid customer ID. Please enter a number.")
