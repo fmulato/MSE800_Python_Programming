1a135b678093ace53a838b0eff517c08645a0ddb
fmulato <fmulato@yahoo.com>
2025-01-20 12:51:41 +1300

    separing code into 2 files: main.py and customer.py

---
+++ customer.py
@@ -0,0 +1,31 @@
+import json
+
+class Customer:
+    def __init__(self, customer_id, name="", email="", phone=""):
+        self._customer_id = customer_id
+        self._name = name
+        self._email = email
+        self._phone = phone
+
+    def create_user(self):
+        self._name = input("Input your full name: ")
+        self._email = input("Input your email: ")
+        self._phone = input("Input your phone number: ")
+
+    def display_info(self):
+        print(f"ID: {self._customer_id}")
+        print(f"Name: {self._name}")
+        print(f"Email: {self._email}")
+        print(f"Phone: {self._phone}")
+
+    def save_json_file(self):
+        data = {
+            "customer_id": self._customer_id,
+            "name": self._name,
+            "email": self._email,
+            "phone": self._phone
+        }
+        print(f"Data saved successfully")
+        print(f"Here are the data:")
+        with open("customer.json", "w") as file:
+            json.dump(data, file)
--- main.py
+++ main.py
@@ -1,43 +1,15 @@
-import json
+import customer
 
-class Customer:
-    def __init__(self, customer_id, name="", email="", phone=""):
-        self._customer_id = customer_id
-        self._name = name
-        self._email = email
-        self._phone = phone
-
-    def create_user(self):
-        self._name = input("Input your full name: ")
-        self._email = input("Input your email: ")
-        self._phone = input("Input your phone number: ")
-
-    def display_info(self):
-        print(f"ID: {self._customer_id}")
-        print(f"Name: {self._name}")
-        print(f"Email: {self._email}")
-        print(f"Phone: {self._phone}")
-
-    def save_json_file(self):
-        data = {
-            "customer_id": self._customer_id,
-            "name": self._name,
-            "email": self._email,
-            "phone": self._phone
-        }
-        print(f"Data saved successfully")
-        print(f"Here are the data:")
-        with open("customer.json", "w") as file:
-            json.dump(data, file)
-
-
-if __name__ == '__main__':
+def run_app():
     try:
         customer_id = int(input("Input your customer ID: "))
-        cust = Customer(customer_id)
+        cust = customer.Customer(customer_id)
         cust.create_user()
         cust.save_json_file()
         cust.display_info()
         print("Thank you for using our system.")
     except ValueError:
         print("Invalid customer ID. Please enter a number.")
+
+if __name__ == '__main__':
+    run_app()
